9b8ec03b59d2e7b2e260cc1ac78ddee85e2c4551
disulfiram <plamen.aaylasecura@gmail.com>
2013-03-18 20:35:47 +0200

    mk10 - HW 5 Complete
    Completed 3rd and final task

---
+++ 4. OOP Pricniples P2/03. Exception/InvalidRangeException.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception
+{
+    class InvalidRangeException<T> : ApplicationException
+    {
+        private T start;
+        private T end;
+
+        public T Start
+        {
+            get
+            {
+                return this.start;
+            }
+            set
+            {
+                this.start = value;
+            }
+        }
+
+        public T End
+        {
+            get 
+            {
+                return this.end;
+            }
+            set
+            {
+                this.end = value;
+            }
+        }
+
+        public InvalidRangeException(T start, T end, string msg)
+            : base(msg)
+        {
+            this.Start = start;
+            this.End = end;
+        }
+    }
+}
